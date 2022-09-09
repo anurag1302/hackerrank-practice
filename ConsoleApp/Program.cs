a4347bd4557f01501e88737536a87ac1a7de612e
Anurag Sinha <anurag3487@gmail.com>
2022-09-09 14:25:28 +0530

    Absolute sum problem

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -26,6 +26,18 @@
                 arr[i] = arr[i] * length;
             }
             Console.WriteLine(arr);
+
+            //Absolute sum problem
+            int[] arr1 = { 2, -1, 4, 8, 10 };
+            var sum = 0;
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                sum = sum + Math.Abs(arr1[i]);
+            }
+            //via LINQ
+            var sum1 = arr1.Sum(x => Math.Abs(x));
+            Console.WriteLine(sum);
+            Console.WriteLine(sum1);
             Console.ReadLine();
         }
     }
