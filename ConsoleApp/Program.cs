9147223de345f3d146df4eb47092ad13a7778993
Anurag Sinha <anurag3487@gmail.com>
2022-09-09 14:08:34 +0530

    Hamming Distance Problme

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //Hamming Distance Problem
+            var str1 = "abcde";
+            var str2 = "abcde";
+            var count = 0;
+
+            for (int i = 0; i < str1.Length; i++)
+            {
+                if (str1[i] != str2[i])
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+            Console.ReadLine();
+        }
+    }
+}
